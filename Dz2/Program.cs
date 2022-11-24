500fb813665d494a2f58a7f6c88d0313107a2f97
SergeySeroff <harek20@mail.ru>
2022-11-24 14:29:40 +0300

    Dz2

---
+++ Dz2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+int min = a;
+
+if (a > max) max = a;
+if (b > max) 
+{
+    max = b;
+}
+else min = b;
+
+Console.WriteLine($"максимальное число {max} минимальное число {min}");
